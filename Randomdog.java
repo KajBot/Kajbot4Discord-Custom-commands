942355388a4c13d26cb08052962d892c3e0e8e28
Jens Møller <jens@jensz12.com>
2019-05-12 00:45:13 +0200

    Create Randomdog.java

---
+++ Randomdog.java
@@ -0,0 +1,34 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.json.JSONObject;
+import support.kajstech.kajbot.command.Command;
+import support.kajstech.kajbot.command.CommandEvent;
+import support.kajstech.kajbot.handlers.ConfigHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Randomdog extends Command {
+    public Randomdog() {
+        this.name = "randomdog";
+        this.guildOnly = false;
+        this.requiredRole = ConfigHandler.getProperty("Bot admin role");
+    }
+
+
+    @Override
+    public void execute(CommandEvent e) {
+        EmbedBuilder eb = new EmbedBuilder();
+        try (Stream<String> stream = new BufferedReader(new InputStreamReader(new URL("https://dog.ceo/api/breeds/image/random").openStream(), StandardCharsets.UTF_8)).lines()) {
+            eb.setImage(new JSONObject(stream.collect(Collectors.joining(System.lineSeparator()))).getString("message"));
+        } catch (IOException ignored) {
+        }
+
+        e.reply(eb.build());
+    }
+
+}

2d6164abd4acf4c827c4cfb9d037423a7d796d12
Jens Møller <jens@jensz12.com>
2019-05-13 21:13:12 +0200

    Update Randomcat.java

--- Randomcat.java
+++ Randomcat.java
@@ -13,9 +13,9 @@ import java.nio.file.StandardCopyOption;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Randomcat extends Command {
-    public Randomcat() {
-        this.name = "catto";
+public class Randomdog extends Command {
+    public Randomdog() {
+        this.name = "doggo";
         this.guildOnly = false;
         this.requiredRole = ConfigHandler.getProperty("Bot admin role");
     }
@@ -23,12 +23,15 @@ public class Randomcat extends Command {
 
     @Override
     public void execute(CommandEvent e) {
-        try (Stream<String> stream = new BufferedReader(new InputStreamReader(new URL("http://aws.random.cat/meow").openStream(), StandardCharsets.UTF_8)).lines()) {
-            URL url = new URL(new JSONObject(stream.collect(Collectors.joining(System.lineSeparator()))).getString("file"));
-
+        try (Stream<String> stream = new BufferedReader(new InputStreamReader(new URL("https://random.dog/woof.json").openStream(), StandardCharsets.UTF_8)).lines()) {
+            URL url = new URL(new JSONObject(stream.collect(Collectors.joining(System.lineSeparator()))).getString("url"));
+            int lastIndexOf = url.toString().lastIndexOf(".");
+            if (lastIndexOf == -1) {
+                return;
+            }
             InputStream in = url.openStream();
-            Files.copy(in, Paths.get(System.getProperty("user.dir") + "/catto" + (url.toString().substring(url.toString().length() - 4))), StandardCopyOption.REPLACE_EXISTING);
-            e.getChannel().sendFile(new File(System.getProperty("user.dir") + "/catto" + url.toString().substring(url.toString().length() - 4))).queue();
+            Files.copy(in, Paths.get(System.getProperty("user.dir") + "/doggo" + name.substring(lastIndexOf)), StandardCopyOption.REPLACE_EXISTING);
+            e.getChannel().sendFile(new File(System.getProperty("user.dir") + "/doggo" + name.substring(lastIndexOf))).queue();
         } catch (IOException ignored) {
         }
     }

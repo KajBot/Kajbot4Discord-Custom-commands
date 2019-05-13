2512d99357365c911dec5db3be83df2976d70cd6
Jens Møller <jens@jensz12.com>
2019-05-13 21:14:04 +0200

    Update Randomdog.java

--- Randomdog.java
+++ Randomdog.java
@@ -25,10 +25,13 @@ public class Randomdog extends Command {
     public void execute(CommandEvent e) {
         try (Stream<String> stream = new BufferedReader(new InputStreamReader(new URL("https://random.dog/woof.json").openStream(), StandardCharsets.UTF_8)).lines()) {
             URL url = new URL(new JSONObject(stream.collect(Collectors.joining(System.lineSeparator()))).getString("url"));
-
+            int lastIndexOf = url.toString().lastIndexOf(".");
+            if (lastIndexOf == -1) {
+                return;
+            }
             InputStream in = url.openStream();
-            Files.copy(in, Paths.get(System.getProperty("user.dir") + "/doggo" + (url.toString().substring(url.toString().length() - 4))), StandardCopyOption.REPLACE_EXISTING);
-            e.getChannel().sendFile(new File(System.getProperty("user.dir") + "/doggo" + url.toString().substring(url.toString().length() - 4))).queue();
+            Files.copy(in, Paths.get(System.getProperty("user.dir") + "/doggo" + name.substring(lastIndexOf)), StandardCopyOption.REPLACE_EXISTING);
+            e.getChannel().sendFile(new File(System.getProperty("user.dir") + "/doggo" + name.substring(lastIndexOf))).queue();
         } catch (IOException ignored) {
         }
     }
